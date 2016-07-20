b37af74fa24588376acf8ab508c38070b76bdefa
Matt Hamil <j.matthew.hamil@gmail.com>
2016-07-20 10:46:59 -0500

    Set up Monopoly for gh-pages deployment

--- scripts/web-server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var rootPath = path.normalize(__dirname + '/../');
-
-app.use(express.static( rootPath + '/app'));
-
-app.listen(8000);
-console.log('Listening on port ' + 8000 + '...');
